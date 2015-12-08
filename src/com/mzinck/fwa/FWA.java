8a51497429949bf00cbd8854ca99e0a4bcbf4545
Mitchell <mitchellzinck@yahoo.com>
2015-12-08 13:07:57 -0400

    Finishing touches

--- src/com/mzinck/fwa/FWA.java
+++ src/com/mzinck/fwa/FWA.java
@@ -1,9 +1,9 @@
 package com.mzinck.fwa;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Scanner;
@@ -22,14 +22,19 @@ public class FWA {
     private static Scanner      scan      = new Scanner(System.in);
     private static int          timeCount = 0,
                                 hardTime = 0;
-    private static OutputStream output;
+    private static File output;
 
     public static void main(String[] args) throws IOException {
-        output = new FileOutputStream(System.getProperty("user.home") + "arithmetic.cfg");
-        props.load(new FileInputStream(System.getProperty("user.home") + "arithmetic.cfg"));
+        output = new File(System.getProperty("user.home") + "\\arithmetic.properties");
+        props.load(new FileInputStream(System.getProperty("user.home") + "\\arithmetic.properties"));
         if(props.getProperty("timeCount") != null) {
             timeCount = Integer.parseInt(props.getProperty("timeCount"));
             hardTime = Integer.parseInt(props.getProperty("hardTime"));
+            name = props.getProperty("name");
+        } else {
+            log("What is your name?");
+            name = scan.nextLine();
+            setProps();
         }
         setState();        
     }
@@ -40,11 +45,12 @@ public class FWA {
     
     public static void setState() {
         state = State.START;
-        System.out.println("Welcome to Fun with Arithmetic " + name + "!\nPlease enter the letters:\n"
+        log("Welcome to Fun with Arithmetic " + name + "!\nPlease enter the letters:\n"
                 + "'N' = New users press this!\n"
                 + "'T' = Time Attack Mode\n"
                 + "'L' = Learner Mode\n"
                 + "'H' = Hard Mode\n"
+                + "'HS' = Check your Highscores\n"
                 + "'X' = Exit Program"); 
         switch(scan.nextLine()) {
             case "N":
@@ -62,7 +68,17 @@ public class FWA {
             case "X":
                 System.exit(0);
                 break;
-                
+            case "HS":
+                log(name + ", your highscores are:\n"
+                        + "Time Attack: " + timeCount + " questions!\n"
+                        + "Hard Mode: " + hardTime + " seconds!\n"
+                        + "Press any key to go back to the main menu!");
+                scan.nextLine();
+                for(int i = 0; i < 50; i++) {
+                    System.out.println();
+                }
+                break;
+               
             default:                
                 log("Wrong input please try again!");
                 break;
@@ -90,7 +106,9 @@ public class FWA {
                         + "Lets get started with the tutorial! We will give you 3 starter questions\n"
                         + "and then you can jump into the full experience!\n");
                 
-                submitQuestion(getQuery(4, false));                    
+                submitQuestion(getQuery(4, false));           
+                log("You completed the tutorial, press any key to continue!");
+                scan.nextLine();
                 break;
             case HARD:
                 log("In Hard mode you are given 5 questions that you have to complete as fast as possible.\n"
@@ -99,13 +117,16 @@ public class FWA {
                 long time = System.currentTimeMillis();
                 submitQuestion(getQuery(5, true));
                 time = System.currentTimeMillis() - time;
-                if(hardTime < time) {
+                if(time < hardTime * 1000) {
                     log("Congratulations you beat your highscore! 5 hard questions in " + (time / 1000) + " seconds!");
                     hardTime = (int) (time / 1000);
                     setProps();
                 } else {
                     log("You completed 5 hard questions in " + (time / 1000) + " seconds!");
                 }
+                log("Press any key to continue!");
+                scan.nextLine();
+                scan.nextLine();
                 break;
             case LEARNER:
                 log("In Learner mode you are given a infinite number of medium questions. Type -1000 to quit.");
@@ -114,6 +135,8 @@ public class FWA {
                     c++;
                 }
                 log("You completed " + c + " questions!");
+                log("Press any key to continue!");
+                scan.nextLine();
                 break;
             case TIME:
                 log("In Time Attack mode you will try to do as many questions as possible in 30 seconds.");
@@ -130,7 +153,12 @@ public class FWA {
                 } else {
                     log("Congratulations, you completed " + counter + " questions in 30 seconds!");
                 }
+                log("Press any key to continue!");
+                scan.nextLine();
                 break;          
+        }
+        for(int i = 0; i < 50; i++) {
+            System.out.println();
         }
         setState();        
     }
@@ -138,6 +166,15 @@ public class FWA {
     public static void setProps() {
         props.setProperty("hardTime", Integer.toString(hardTime));
         props.setProperty("timeCount", Integer.toString(timeCount));
+        props.setProperty("name", name);
+        try {
+            FileWriter writer = new FileWriter(output);
+            props.store(writer, null);
+            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     public static void countDown() {
@@ -161,25 +198,31 @@ public class FWA {
         Random rand = new Random();
         String query = "What is ";
         int total = 0;        
-        int indexes = rand.nextInt(3) + 2;
-        
-        int z = hard ? rand.nextInt(50) + 1 : rand.nextInt(20) + 1;
-        total += z;
-        query += z;
+        int indexes = rand.nextInt(2) + 2;
+        int z;        
         
         for(int e = 0; e < totalQ; e++) {
+            if(hard == true) {            
+                z = rand.nextInt(4) + 1;
+            } else {
+                z = rand.nextInt(1) + 1;
+            }
+            total += z;
+            query += new String(new char[z]).replace("\0", "*");
             for(int i = 0; i < indexes; i++) {
                 if(rand.nextInt(2) == 0) {
-                    z = rand.nextInt(50) + 1;
-                    query += " - " + z;
+                    z = hard == true ? rand.nextInt(4) + 1 : rand.nextInt(2) + 1;
+                    query += " - " + new String(new char[z]).replace("\0", "*");
                     total -= z;
                 } else {
-                    z = rand.nextInt(50) + 1;
-                    query += " + " + z;
+                    z = hard == true ? rand.nextInt(4) + 1 : rand.nextInt(2) + 1;
+                    query += " + " + new String(new char[z]).replace("\0", "*");
                     total += z;
                 }            
             }
             array[e] = query + "?." + total;
+            query = "What is ";
+            total = 0;
         }
         
         return array;
@@ -187,7 +230,11 @@ public class FWA {
     
     public static boolean submitQuestion(String[] query) {
         for(int i = 0; i < query.length; i++) {
-            log("Question " + (i + 1) + ": " + query[i].substring(0, query[i].indexOf(".")));
+            if(query.length > 1) {
+                log("Question " + (i + 1) + ": " + query[i].substring(0, query[i].indexOf(".")));
+            } else {
+                log("Question: " + query[i].substring(0, query[i].indexOf(".")));
+            }
             int a;
             while((a = scan.nextInt()) != Integer.parseInt(query[i].split("\\.")[1])) {
                 if(a == -1000) {
